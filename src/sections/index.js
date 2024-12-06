7ef82b805e6faef5b1acde346256df373091b545
Patrick Beal <patrickbeal.calagahan009@gmail.com>
2024-12-06 20:17:00 +0800

    configure react app for github pages

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import PopularProducts from './PopularProducts'
+import Hero from './Hero'
+import CustomerReviews from './CustomerReviews'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import Footer from './Footer'
+
+export{
+
+    Hero, 
+    PopularProducts,
+    CustomerReviews,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    Services,
+    Footer
+}
